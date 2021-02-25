5a7bfff2069fd902063b8ed0c370cebae2b20e97
Patrick <w.patrick.kelly@gmail.com>
2021-02-24 16:34:12 -0800

    Initial Commit

---
+++ Brain Games/Brain_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brain_GamesApp.swift
+//  Brain Games
+//
+//  Created by patrick kelly on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct Brain_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Brain Games/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Brain Games
+//
+//  Created by patrick kelly on 2/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
