4e64ce22bf0a75d571ec16e598b01f7bdbd69b2e
Patrick <w.patrick.kelly@gmail.com>
2021-02-25 20:03:42 -0800

    first commit

---
+++ Brain Games/GlobalState.swift
@@ -0,0 +1,8 @@
+//
+//  GlobalState.swift
+//  Brain Games
+//
+//  Created by patrick kelly on 2/24/21.
+//
+
+import Foundation
---
+++ Brain Games/WordKey.swift
@@ -0,0 +1,8 @@
+//
+//  WordKey.swift
+//  Brain Games
+//
+//  Created by patrick kelly on 2/25/21.
+//
+
+import Foundation
